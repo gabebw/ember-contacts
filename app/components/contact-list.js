534f938fdf8aab5a83e8b2789c8dd54f798cab61
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-26 14:20:25 -0400

    Users can type to filter a list of contacts

--- app/components/contact-list.js
+++ app/components/contact-list.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  // `query` is an arbitrary name
+  query: '',
+
+  // If query changes, recompute this property, AND
+  // if any of the contacts' fullNames change, recompute as well
+  filteredContacts: Ember.computed(
+    'query',
+    'contacts.@each.fullName',
+    function(){
+      const query = this.get('query');
+      const contacts = this.get('contacts');
+      // turn the query into a case-insensitive query
+      const caseInsensitiveQuery = new RegExp(query, "i");
+
+      return contacts.filter(function(contact){
+        return contact.get('fullName').match(caseInsensitiveQuery);
+      });
+    }
+  ),
 });
