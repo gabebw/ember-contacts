7c94093137c4a2220b37557359ac454973839081
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-26 16:34:21 -0400

    Users can destroy contacts

--- app/routes/contacts/contact.js
+++ app/routes/contacts/contact.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    destroy: function(){
+      this.get('currentModel').destroyRecord();
+      this.transitionTo('contacts');
+    }
+  }
 });
