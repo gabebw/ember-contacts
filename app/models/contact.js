d768289d61bbf73a5df95b5d71ebf0fa76d63da6
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-26 13:55:05 -0400

    Add a computed fullName property to Contact model

--- app/models/contact.js
+++ app/models/contact.js
@@ -5,4 +5,8 @@ export default DS.Model.extend({
   nameLast: DS.attr('string'),
   email: DS.attr('string'),
   twitter: DS.attr('string'),
+  // Computed properties change when the properties they depend on change
+  fullName: Ember.computed('nameFirst', 'nameLast', function(){
+    return `${this.get('nameFirst')} ${this.get('nameLast')}`;
+  }),
 });
