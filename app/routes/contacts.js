673759375ab21124341608aef4d2ab2f1aa35d44
Gabe Berke-Williams <gabebw@gabebw.com>
2015-06-26 13:51:23 -0400

    Show first name/last name in contacts template

--- app/routes/contacts.js
+++ app/routes/contacts.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  // The `model` hook is like a before_action in Rails - it runs when the route
+  // is loaded.
+  model: function(){
+    return this.store.findAll('contact');
+  }
 });
